017c4d4a4857ff2d5e49ddcfdc07b0225d2e9065
Bryan Duff <bduff@ecessa.com>
2013-01-09 10:54:37 -0600

    initial commit - all files

---
+++ led/include/version.h
@@ -0,0 +1,3 @@
+#define PLATFORM_NAME "MB-7540"
+#define CODE_VERSION "V1.0 2011-11-2"
+
---
+++ wd_bp/include/version.h
@@ -0,0 +1,8 @@
+#define PLATFORM_NAME "MB-7540"
+#define CODE_VERSION "V1.0 2011-11-02"
+
+// HW configuration
+// define BYPASS_PAIR_NUMBER is necessary
+// define BYPASS_PAIR_NUMBER to 0 for no LAN-bypass available
+#define BYPASS_PAIR_NUMBER 2
+
